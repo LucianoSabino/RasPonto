e0bd4cbad4761b37111c4e02f0888921c65f0e34
Luciano Sabino <lb255043@gmail.com>
2025-03-28 04:19:34 -0300

    Atualização

--- src/server/database/migrations/0000_creat_usuario.js
+++ src/server/database/migrations/0000_creat_usuario.js
@@ -14,6 +14,8 @@ export const up = function (knex) {
     table.string("senha").notNullable();
     table.string("membresia").notNullable();
     table.string("role").notNullable();
+    table.string("robo").notNullable();
+    table.string("sobre").notNullable();
     table.date("data").defaultTo(knex.raw("CURRENT_DATE")).notNullable();
   });
 };
