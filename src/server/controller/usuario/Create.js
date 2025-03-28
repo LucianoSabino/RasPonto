914bab8c7145f0e53818c120bc6110f82fb1abe7
Luciano Sabino <lb255043@gmail.com>
2025-03-28 02:24:55 -0300

    atualização algumas variavel para caixa alta

--- src/server/controller/usuario/Create.js
+++ src/server/controller/usuario/Create.js
@@ -7,7 +7,7 @@ export const createValidation = validation((getSchema) => ({
   body: getSchema(
     yup.object().shape({
       nome: yup.string().required().min(3).max(255),
-      matricula: yup.string().required(),
+      matricula: yup.string().required().min(10).max(10),
       curso: yup.string().required(),
       telefone: yup.string().required(),
       email: yup.string().required().email(),
@@ -19,7 +19,20 @@ export const createValidation = validation((getSchema) => ({
 }));
 
 export const createUp = async (req, res) => {
-  const result = await UsuarioProvider.create(req.body);
+  const { nome, matricula, curso, telefone, email, senha, membresia, role } =
+    req.body;
+
+  const data = {
+    nome: nome.toUpperCase(),
+    matricula: matricula.toString(),
+    curso: curso.toUpperCase(),
+    telefone: telefone.toUpperCase(),
+    email,
+    senha,
+    membresia,
+    role,
+  };
+  const result = await UsuarioProvider.create(data);
   console.log(result);
   if (result instanceof Error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
