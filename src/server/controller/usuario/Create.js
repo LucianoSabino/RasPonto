26793632844a5b72f8f286c855e09a6e22e41e17
Luciano Sabino <lb255043@gmail.com>
2025-03-28 03:56:55 -0300

    add novos campos, trasnformando caixa alta, alterando tamanho maxino de string

--- src/server/controller/usuario/Create.js
+++ src/server/controller/usuario/Create.js
@@ -6,7 +6,7 @@ import { UsuarioProvider } from "../../providers/usuario/index.js";
 export const createValidation = validation((getSchema) => ({
   body: getSchema(
     yup.object().shape({
-      nome: yup.string().required().min(3).max(255),
+      nome: yup.string().required().min(3).max(60),
       matricula: yup.string().required().min(10).max(10),
       curso: yup.string().required(),
       telefone: yup.string().required(),
@@ -21,16 +21,19 @@ export const createValidation = validation((getSchema) => ({
 export const createUp = async (req, res) => {
   const { nome, matricula, curso, telefone, email, senha, membresia, role } =
     req.body;
-
+  const robo = "NÃO INFORMADO!";
+  const sobre = "NÃO INFORMADO!";
   const data = {
-    nome: nome.toUpperCase(),
-    matricula: matricula.toString(),
-    curso: curso.toUpperCase(),
-    telefone: telefone.toUpperCase(),
+    nome: nome ? nome.toUpperCase() : "", // Converte nome para caixa alta
+    matricula,
+    curso: curso ? curso.toUpperCase() : "", // Converte curso para caixa alta
+    telefone,
     email,
     senha,
     membresia,
     role,
+    robo: robo ? robo.toUpperCase() : "",
+    sobre: sobre ? sobre.toUpperCase() : "",
   };
   const result = await UsuarioProvider.create(data);
   console.log(result);
